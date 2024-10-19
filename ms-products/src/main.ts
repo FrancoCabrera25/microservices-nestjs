94e4e3f4fb6db4944e77fd392af42328fe72d8d7
Franco Cabrera <cabrera.franco@outlook.com>
2024-10-19 19:07:11 -0300

    add config init and db

--- ms-products/src/main.ts
+++ ms-products/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port ?? 3000);
+  console.log(`app runnig ${envs.port}`)
+
 }
 bootstrap();
