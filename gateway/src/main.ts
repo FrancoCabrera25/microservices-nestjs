0dd1317cd8020b2ba7176e12208621f08656cc2c
Franco Cabrera <cabrera.franco@outlook.com>
2024-10-29 19:32:04 -0300

    integration microservice products

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
   const logger = new Logger('Main-gateway');
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalFilters(new RpcCustomExceptionFilter());
   app.setGlobalPrefix('api');
   await app.listen(envs.port);
 
--- gateway/src/products/products.controller.ts
+++ gateway/src/products/products.controller.ts
@@ -3,23 +3,33 @@ import {
   Controller,
   Delete,
   Get,
+  Inject,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
 
 @Controller('products')
 export class ProductsController {
-  constructor() {}
+  constructor(@Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy) {}
 
   @Post()
   createProduct() {}
 
   @Get()
-  findProducts() {}
+  findAlloducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send({cmd: 'find_all_product'}, paginationDto);
+  }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {}
+  findOne(@Param('id') id: string) {
+    return this.productsClient.send({cmd: 'find_one_product'}, { id });
+  }
 
   @Delete(':id')
   deleteProduct(@Param('id') id: string) {}
