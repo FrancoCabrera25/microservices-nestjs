7760c2ba7edb77a14d4bf8e3efaa9e98b57ceb18
Franco Cabrera <cabrera.franco@outlook.com>
2025-01-18 00:08:29 -0300

    autjh ms

---
+++ ms-auth/src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class AuthService extends PrismaClient implements OnModuleInit {
+
+    private readonly logger = new Logger("AuthSerice");
+
+    onModuleInit() {
+        this.$connect();
+        this.logger.log('MongoDB connected');
+    }
+
+}
